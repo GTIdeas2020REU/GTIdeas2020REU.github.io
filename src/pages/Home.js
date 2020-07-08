2db13868777acf25b6be61acbb90e20f4dbbe485
Rachel Ombok <rbo224@nyu.edu>
2020-07-08 17:18:59 -0400

    fixing website navigation

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Home = (props) => (
+  <header className="masthead">
+  <div className="container">
+    <div className="intro-text">
+      <div className="intro-lead-in">We are Georgia Tech 2020 IDEAS REU.</div>
+      {/*<div className="intro-heading text-uppercase">It's Nice To Meet You</div>*/}
+      {/*<a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>*/}
+    </div>
+  </div>
+</header>
+
+)
