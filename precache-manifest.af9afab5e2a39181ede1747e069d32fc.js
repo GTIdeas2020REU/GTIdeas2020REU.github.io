72c10b56b0a5441b528adeee6ad8ed789f7800aa
Rachel Ombok <rbo224@nyu.edu>
2020-08-03 23:40:04 -0400

    Updates

---
+++ precache-manifest.af9afab5e2a39181ede1747e069d32fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "419b3b88a2f78e8945670354f6acc955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a596d47f8b5fef812ea8",
+    "url": "/static/css/main.c11f0e74.chunk.css"
+  },
+  {
+    "revision": "22eebc70f55ddc5f01a9",
+    "url": "/static/js/2.bd73f998.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.bd73f998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a596d47f8b5fef812ea8",
+    "url": "/static/js/main.3a1714d1.chunk.js"
+  },
+  {
+    "revision": "b782c8f5c1804d0a8711",
+    "url": "/static/js/runtime-main.30305a7d.js"
+  },
+  {
+    "revision": "a16e02041ed6fdfdea1a87a6fdfd7d3b",
+    "url": "/static/media/coronabg.a16e0204.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/static/media/map-image.f40ed5b1.png"
+  },
+  {
+    "revision": "780024aa49d2819ad2960e6825ea0099",
+    "url": "/static/media/modelbg.780024aa.png"
+  },
+  {
+    "revision": "acb5adac8ebf5f05033192e7f5b54a14",
+    "url": "/static/media/person-1.acb5adac.jpg"
+  }
+]);
